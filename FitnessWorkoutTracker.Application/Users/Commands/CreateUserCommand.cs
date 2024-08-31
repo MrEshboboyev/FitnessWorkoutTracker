eaca4a806a754f3ce3e95952ce3186a4b0d90a23
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-01 00:21:23 +0500

    Create SQRS -> Create User Command : Application : Create Users/Commands/CreateUserCommand.cs : inheirt from IRequest<UserDTO>, fields (Username, Email, Password); Adding Package MediatR;

---
+++ FitnessWorkoutTracker.Application/Users/Commands/CreateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using FitnessWorkoutTracker.Application.DTOs;
+using MediatR;
+
+namespace FitnessWorkoutTracker.Application.Users.Commands
+{
+    public class CreateUserCommand : IRequest<UserDTO>
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
