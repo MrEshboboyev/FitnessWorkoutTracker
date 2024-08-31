d42cd3ed767f39af2c12ed33ffcb4d127b81c0df
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-31 23:51:30 +0500

    Repositories writed for entities in Application/Interfaces;

---
+++ FitnessWorkoutTracker.Application/Interfaces/ICommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using FitnessWorkoutTracker.Domain.Entities;
+
+namespace FitnessWorkoutTracker.Application.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<Comment> GetByIdAsync(Guid id);
+        Task<IEnumerable<Comment>> GetByWorkoutIdAsync(Guid workoutId);
+        Task AddAsync(Comment Comment);
+        Task UpdateAsync(Comment Comment);
+        Task RemoveAsync(Guid id);
+    }
+}
---
+++ FitnessWorkoutTracker.Application/Interfaces/IExerciseRepository.cs
@@ -0,0 +1,13 @@
+﻿using FitnessWorkoutTracker.Domain.Entities;
+
+namespace FitnessWorkoutTracker.Application.Interfaces
+{
+    public interface IExerciseRepository
+    {
+        Task<Exercise> GetByIdAsync(Guid id);
+        Task<IEnumerable<Exercise>> GetByWorkoutIdAsync(Guid workoutId);
+        Task AddAsync(Exercise Exercise);
+        Task UpdateAsync(Exercise Exercise);
+        Task RemoveAsync(Guid id);
+    }
+}
