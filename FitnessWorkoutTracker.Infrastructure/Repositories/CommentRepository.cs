bf8bd7737e32ed0d84e1dce833d569be01323c0a
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-01 11:21:48 +0500

    Comment repo implemented : Infrastructure : Create Repositories/CommentRepository.cs : inherit from ICommentRepository, inject DB, methods implemented;

---
+++ FitnessWorkoutTracker.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,51 @@
+﻿using FitnessWorkoutTracker.Application.Interfaces;
+using FitnessWorkoutTracker.Domain.Entities;
+using FitnessWorkoutTracker.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessCommentTracker.Infrastructure.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly AppDbContext _db;
+
+        public CommentRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(Comment comment)
+        {
+            await _db.Comments.AddAsync(comment);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<Comment> GetByIdAsync(Guid id)
+        {
+            return await _db.Comments.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Comment>> GetByWorkoutIdAsync(Guid workoutId)
+        {
+            return await _db.Comments
+                .Where(c => c.WorkoutId == workoutId)
+                .ToListAsync();
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            var comment = await GetByIdAsync(id);
+            if (comment != null)
+            {
+                _db.Comments.Remove(comment);
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateAsync(Comment comment)
+        {
+            _db.Comments.Update(comment);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
