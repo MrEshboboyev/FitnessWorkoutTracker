cafcac09d3abba5a6de822bfbdfd8a01577d3c0c
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-01 11:06:49 +0500

    user repo implemented : Infrastructure : Create Repositories/UserRepository.cs : inherit from IUserRepository, inject DB, methods implemented;

---
+++ FitnessWorkoutTracker.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using FitnessWorkoutTracker.Application.Interfaces;
+using FitnessWorkoutTracker.Domain.Entities;
+using FitnessWorkoutTracker.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessWorkoutTracker.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _db;
+
+        public UserRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<User> GetByIdAsync(Guid id)
+        {
+            return await _db.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetByUsernameAsync(string userName)
+        {
+            return await _db.Users.SingleOrDefaultAsync(u => u.UserName == userName);
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            var user = await GetByIdAsync(id);
+            if (user != null)
+            {
+                _db.Users.Remove(user);
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _db.Users.Update(user);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
