e3778a034d8c83b791b9ca19bde63938ad1b55ca
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-01 16:42:07 +0500

    Configure JWT Authentication : Presentation : Adding Package : Microsoft.AspNetCore.Authentication.JwtBearer; Program.cs : add authentication with JWT bearer;

--- FitnessWorkoutTracker.Presentation/Program.cs
+++ FitnessWorkoutTracker.Presentation/Program.cs
@@ -3,8 +3,11 @@ using FitnessWorkoutTracker.Application.Services.Interfaces;
 using FitnessWorkoutTracker.Domain.Entities;
 using FitnessWorkoutTracker.Infrastructure.Data;
 using FitnessWorkoutTracker.Infrastructure.Implementations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +27,28 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
+
+// configure JWT Authentication
+var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtSettings:Key"]);
+var issuer = builder.Configuration["JwtSettings:Issuer"];
+var audience = builder.Configuration["JwtSettings:Audience"];
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience
+    };
+});
+
 
 // adding lifetime for services
 builder.Services.AddScoped<IDbInitializer, DbInitializer>();
