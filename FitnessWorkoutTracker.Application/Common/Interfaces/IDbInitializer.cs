f80c068e9d0635c5bfe89de8b3c3f2f50964aa71
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-01 13:49:06 +0500

    create DbInitializer interface and implemented : Application : CreateCommon/Interfaces/IDbInitializer.cs : Initialize(); Create Common/Utility/SD.cs : static class and inside roles; Infrastructure : Create Data/DbInitializer.cs : inherit from IDbInitializer, inject Identity Managers and Db, method implemented;

---
+++ FitnessWorkoutTracker.Application/Common/Interfaces/IDbInitializer.cs
@@ -0,0 +1,7 @@
+﻿namespace FitnessWorkoutTracker.Application.Common.Interfaces
+{
+    public interface IDbInitializer
+    {
+        void Initialize();
+    }
+}
---
+++ FitnessWorkoutTracker.Infrastructure/Data/DbInitializer.cs
@@ -0,0 +1,66 @@
+﻿using FitnessWorkoutTracker.Application.Common.Interfaces;
+using FitnessWorkoutTracker.Application.Common.Utility;
+using FitnessWorkoutTracker.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessWorkoutTracker.Infrastructure.Data
+{
+    public class DbInitializer : IDbInitializer
+    {
+        // inject Identity Managers
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly AppDbContext _db;
+
+        public DbInitializer(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            AppDbContext db)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                // migrate
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+
+                // If "Admin" role does not exist, create admin user and roles
+                if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+                {
+                    // creating roles
+                    _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).Wait();
+                    _roleManager.CreateAsync(new IdentityRole(SD.Role_User)).Wait();
+
+                    // create admin user
+                    _userManager.CreateAsync(new ApplicationUser
+                    {
+                        UserName = "admin@example.com",
+                        Email = "admin@example.com",
+                        FullName = "John Doe",
+                        NormalizedUserName = "ADMIN@EXAMPLE.COM",
+                        NormalizedEmail = "ADMIN@EXAMPLE.COM",
+                        PhoneNumber = "1112223333",
+                    }, "Admin*123").GetAwaiter().GetResult();
+
+                    // finding user and assign role
+                    var admin = _db.Users.FirstOrDefault(u => u.Email == "admin@example.com");
+
+                    _userManager.AddToRoleAsync(admin, SD.Role_Admin).GetAwaiter().GetResult();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+    }
+}
