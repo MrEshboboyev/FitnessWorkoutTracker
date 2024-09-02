10149f919226e36630edc39e23c43922dc4b55ab
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-02 17:05:37 +0500

    Adding IsFinished field to Workout entity : Domain : Entities/Workout.cs : added IsFinished field and default value false; Adding migrations to db;

--- FitnessWorkoutTracker.Domain/Entities/Workout.cs
+++ FitnessWorkoutTracker.Domain/Entities/Workout.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public string UserId { get; set; }
         public DateTime Date { get; set; }
+        public bool IsFinished { get; set; } = false;
 
         // Navigation Properties
         public ApplicationUser User { get; set; }
---
+++ FitnessWorkoutTracker.Infrastructure/Data/Migrations/20240902120343_addIsFinishedToWorkout.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessWorkoutTracker.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIsFinishedToWorkout : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFinished",
+                table: "Workouts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFinished",
+                table: "Workouts");
+        }
+    }
+}
