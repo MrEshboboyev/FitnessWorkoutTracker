686b99c9fa19408481ae8af3b8058f1aeeb9930a
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-02 17:23:26 +0500

    Task 6: Generate a Report of Past Workouts : Application : Create DTOs/WorkoutQueryDTO.cs : UserId, StartDate, EndDate; Application : Services/Interfaces/IWorkoutService.cs : added GetPastWorkoutsAsync(), GetFinishedWorkoutsPercentageAsync();

---
+++ FitnessWorkoutTracker.Application/DTOs/WorkoutQueryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessWorkoutTracker.Application.DTOs
+{
+    public class WorkoutQueryDTO
+    {
+        public string UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
--- FitnessWorkoutTracker.Application/Services/Interfaces/IWorkoutService.cs
+++ FitnessWorkoutTracker.Application/Services/Interfaces/IWorkoutService.cs
@@ -10,6 +10,8 @@ namespace FitnessWorkoutTracker.Application.Services.Interfaces
         Task<IEnumerable<Workout>> GetUserWorkoutsByWorkoutNameAsync(string userId, string workoutName);
         Task<IEnumerable<Workout>> GetWorkoutsByWorkoutNameAsync(string workoutName);
         Task<IEnumerable<Workout>> GetUserActiveWorkoutsAsync(string userId);
+        Task<IEnumerable<Workout>> GetPastWorkoutsAsync(WorkoutQueryDTO workoutQueryDTO);
+        Task<double> GetFinishedWorkoutsPercentageAsync(WorkoutQueryDTO workoutQueryDTO);
         Task CreateUserWorkoutAsync(WorkoutCreateDTO workoutCreateDTO);
         Task UpdateUserWorkoutAsync(WorkoutUpdateDTO workoutUpdateDTO);
         Task RemoveUserWorkoutAsync(WorkoutRemoveDTO workoutRemoveDTO);
